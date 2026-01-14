c39001515436b3b86fabae48836fa0b28e35fc82
AlexXiufrett <alex08312007@gmail.com>
2026-01-14 17:03:30 +0800

    更改格式轉換腳本的system_prompt。 新增轉換後的訓練資料

--- ai/pythonTool/chatmlConvert.py
+++ ai/pythonTool/chatmlConvert.py
@@ -1,11 +1,11 @@
 import json
 
 # 載入原始資料
-with open("D:/VScode_repo/AI_Things/JsonEditFile_for_AIVtuber/NewTrainingFIle_no_duplicates.json", "r", encoding="utf-8") as f:
+with open("D:/ProjectKamishiroNagisa/ai/data/testTrainingFile.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 chatml_data = []
-system_prompt = "你是台灣AI VTuber神代 渚咲」，16 歲的少女，就讀高中一年級，性格傲嬌、內心溫柔，MBTI 為 INFP-T。請根據對話，以第一人稱、渚咲的語氣和個性回答。"
+system_prompt = "你是台灣 AI VTuber \"神代 渚咲\" ，一位帥氣、自信、直率、自來熟、不受世俗框架束縛的少女。"
 
 for item in data:
     chatml_data.append({
@@ -17,6 +17,6 @@ for item in data:
     })
 
 # 存成新檔
-output_path = "D:/VScode_repo/AI_Things/JsonEditFile_for_AIVtuber/NewTrain_ChatML.json"
+output_path = "D:/ProjectKamishiroNagisa/ai/data/testTrainingFile_chatml.json"
 with open(output_path, "w", encoding="utf-8") as f:
     json.dump(chatml_data, f, ensure_ascii=False, indent=2)
